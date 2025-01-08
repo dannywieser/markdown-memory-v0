d2fd25cd8aa0afe2baf47f74b1ba5a7ae744c2b8
Danny Wieser <danny.wieser@gmail.com>
2025-01-08 07:58:26 -0700

    add error handling to copy

--- src/bear/copy.ts
+++ src/bear/copy.ts
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 import * as fs from 'fs'
-import { info } from 'src/utils'
+import { error, info } from '../utils'
 
 const sourceFile =
   '/Users/dgw/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data/database.sqlite'
@@ -10,7 +10,11 @@ const destDir = './db-backup'
 export default function copyBearDatabase() {
   const currentDate = format(new Date(), 'yyyy.MM.dd')
   const destFile = `${destDir}/${currentDate}.sqllite`
-  fs.copyFile(sourceFile, destFile, fs.constants.COPYFILE_EXCL, () => {
-    info(`copied: ${destFile}`)
+  fs.copyFile(sourceFile, destFile, fs.constants.COPYFILE_EXCL, (err) => {
+    if (err) {
+      error('copy failed, file already exists')
+    } else {
+      info(`copied: ${destFile}`)
+    }
   })
 }
--- src/utils/log.ts
+++ src/utils/log.ts
@@ -3,9 +3,11 @@ import chalk from 'chalk'
 const blue = chalk.blueBright
 const yellow = chalk.yellowBright
 const green = chalk.greenBright
+const red = chalk.redBright
 
 const line = yellow(' --- ')
 export const info = (message: string) => console.log(` > ${green(message)}`)
 export const log = (message: string) => console.log(blue(message))
+export const error = (message: string) => console.log(red(message))
 export const startup = (message: string) =>
   console.log(`${line}${blue(message)}${line}`)
