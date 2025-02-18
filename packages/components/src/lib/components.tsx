9d8ad307ce88cd6d2a13c9da68f6973aa2354995
Danny Wieser <danny.wieser@gmail.com>
2025-02-18 07:13:48 -0700

    add component library placeholder

---
+++ packages/components/src/lib/components.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+const StyledComponents = styled.div`
+  color: pink;
+`
+export function Components() {
+  return (
+    <StyledComponents>
+      <h1>Welcome to Components!</h1>
+    </StyledComponents>
+  )
+}
+
+export default Components
