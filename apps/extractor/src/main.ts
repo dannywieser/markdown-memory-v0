ff845bfbacf5e62d4d218cc80f6440c5ae88eb52
Danny Wieser <danny.wieser@gmail.com>
2025-02-18 07:47:52 -0700

    todo: basic extractor

--- apps/extractor/src/main.ts
+++ apps/extractor/src/main.ts
@@ -1,1 +1,11 @@
-console.log('Hello World')
+import schedule from 'node-schedule'
+
+function runExtractor() {
+  console.info(`extractor`)
+}
+
+// TODO: configuration for frequency
+schedule.scheduleJob('* * * * *', runExtractor)
+
+// always run once right away
+runExtractor()
