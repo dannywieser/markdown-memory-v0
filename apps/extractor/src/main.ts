dd4228a79cea0df55fead1d01f0d50e3407a8575
Danny Wieser <danny.wieser@gmail.com>
2025-02-20 07:39:27 -0700

    docker container for extractor

--- apps/extractor/src/main.ts
+++ apps/extractor/src/main.ts
@@ -1,5 +1,5 @@
 import schedule from 'node-schedule'
-import { loadConfig, logserve, info } from '@markdown-memory/utilities'
+import { logserve, info } from '@markdown-memory/utilities'
 import bearExtractor from '@markdown-memory/extractor-bear'
 import fileExtractor from '@markdown-memory/extractor-file'
 export const extractorMap = {
@@ -7,15 +7,18 @@ export const extractorMap = {
   file: fileExtractor,
 }
 
+// default schedule if no config is provided: every 5 minutes
+const defaultSchedule = '*/5 * * * *'
+
 const startup = async () => {
-  // default schedule if no config is provided: every 5 minutes
-  const defaultSchedule = '*/5 * * * *'
-  const { extractorType, schedule: scheduleConfig = defaultSchedule } =
-    await loadConfig()
+  const {
+    EXTRACTOR_TYPE: extractorType = 'bear',
+    EXTRACTOR_SCHEDULE: scheduleConfig = defaultSchedule,
+  } = process.env
 
   const extractor = extractorMap[extractorType]
   // setup extractor for configured scheduled runs
-  logserve(`markdown memory: extractor start`)
+  logserve(`markdown memory: extractor`)
   info(`mode: ${extractorType}`)
   info(`schedule: ${scheduleConfig}`)
 
