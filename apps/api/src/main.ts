be4546b05ed114423966dfc3bdf2b9c78619f3b9
Danny Wieser <danny.wieser@gmail.com>
2025-03-04 08:14:58 -0700

    chore: fix api port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -46,7 +46,7 @@ app.get('/api/notes', async (req, res) => {
   res.send(notes)
 })
 
-const port = process.env.PORT || 3334
+const port = process.env.PORT || 3333
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`)
 })
