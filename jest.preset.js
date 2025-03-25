5c407136108bfc1de21b18e6f6aa03abfbe08ecf
Danny Wieser <danny.wieser@gmail.com>
2025-03-25 15:37:10 -0600

    chore: more test config

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,17 @@
 const nxPreset = require('@nx/jest/preset').default
 
-module.exports = { ...nxPreset }
+module.exports = {
+  ...nxPreset,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.tsx',
+    '!src/**/*.types.ts',
+    '!src/**/index.ts',
+    '!src/**/mocks.ts',
+    '!src/**/types.ts',
+    '!src/**/types/**',
+    '!src/**/*.styles.ts',
+  ],
+  coverageReporters: ['cobertura', 'html', 'json', 'text', 'text-summary'],
+}
