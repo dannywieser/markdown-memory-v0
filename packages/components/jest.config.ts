59040d1178a7f18c01c675c991a6387410bb514f
Danny Wieser <danny.wieser@gmail.com>
2025-03-25 15:24:10 -0600

    chore: tests running in all packages, add codecov

--- packages/components/jest.config.ts
+++ packages/components/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: '@markdown-memory/components',
+  displayName: 'components',
   preset: '../../jest.preset.js',
-  testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/components',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/components',
 }
