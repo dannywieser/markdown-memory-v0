2cfc5d89702805960a2dd37f682a8ea966f480e8
Danny Wieser <danny.wieser@gmail.com>
2025-04-04 16:32:30 -0600

    fix: types for TextVariant

--- packages/components/src/theme/theme.ts
+++ packages/components/src/theme/theme.ts
@@ -64,6 +64,7 @@ export const theme: MarkdownMemoryTheme = {
     code: bodyText,
     codespan: bodyText,
     em: bodyText,
+    escape: bodyText,
     del: bodyText,
     strong: {
       ...bodyText,
