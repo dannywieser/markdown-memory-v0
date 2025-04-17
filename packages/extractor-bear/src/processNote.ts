ef9c91b366f3d56710b04fc7a795816004392c10
Danny Wieser <danny.wieser@gmail.com>
2025-04-17 08:57:18 -0600

    test: add tests for processNote

--- packages/extractor-bear/src/processNote.ts
+++ packages/extractor-bear/src/processNote.ts
@@ -3,58 +3,34 @@ import { convertDate } from '@markdown-memory/utilities'
 
 import { BearProcessedFile, BearProcessedTag, BearRawNote } from './types'
 import { generateExternalUrl } from './util'
-
-const processImages = (rawNote: string, noteFiles: BearProcessedFile[]) => {
-  const specialTokensRegex = /!\[[^\]]*\]\((.*?)\s*("(?:.*[^"])")?\s*\)/g
-  const matches = rawNote.matchAll(specialTokensRegex)
-
-  if (!matches) {
-    return rawNote
-  }
-
-  for (const match of matches) {
-    const filename = match[1]
-    const matchedFile = noteFiles.find(({ filename }) => filename === match[1])
-
-    const fullFilePath = matchedFile
-      ? `${matchedFile.fileId}/${matchedFile.filename}`
-      : filename
-
-    rawNote = rawNote.replace(filename, fullFilePath)
-  }
-  return rawNote
-}
+import extractNoteTags from './util/extractNoteTags'
+import fixImagePaths from './util/fixImagePaths'
 
 export default function processNote(
   rawNote: BearRawNote,
   allTags: BearProcessedTag[],
   allFiles: BearProcessedFile[]
 ): MarkdownNote {
-  // 1. retrieve all files associated with the current note and map to the correct types to save with the record
-  const processingNoteId = rawNote.Z_PK
-  const noteFiles = allFiles.filter(({ noteId }) => noteId === processingNoteId)
-  const noteText = processImages(rawNote.ZTEXT, noteFiles)
-
-  // 2. retrieve all tags associated with the current note
-  const tags = allTags
-    // first find tags that have been assigned to this current note ID
-    .filter(({ noteIds }) =>
-      noteIds.some((id: number) => id === processingNoteId)
-    )
-    // then reduce the result down to a string array only containing the tags title
-    .reduce(
-      (titles: string[], { title }: BearProcessedTag) => [...titles, title],
-      []
-    )
+  const {
+    Z_PK: notePrimaryKey,
+    ZUNIQUEIDENTIFIER: noteUniqueId,
+    ZTEXT: rawNoteText,
+    ZCREATIONDATE: creationDate,
+    ZMODIFICATIONDATE: modificationDate,
+    ZTITLE: title,
+  } = rawNote
+  const noteFiles = allFiles.filter(({ noteId }) => noteId === notePrimaryKey)
+  const noteText = fixImagePaths(rawNoteText, noteFiles)
+  const tags = extractNoteTags(notePrimaryKey, allTags)
 
   return {
     tokens: lexer(noteText),
-    created: convertDate(rawNote.ZCREATIONDATE),
-    id: rawNote.ZUNIQUEIDENTIFIER,
-    modified: convertDate(rawNote.ZMODIFICATIONDATE),
+    created: convertDate(creationDate),
+    id: noteUniqueId,
+    modified: convertDate(modificationDate),
     tags,
-    title: rawNote.ZTITLE,
+    title,
     source: 'bear',
-    externalUrl: generateExternalUrl(rawNote.ZUNIQUEIDENTIFIER),
+    externalUrl: generateExternalUrl(noteUniqueId),
   }
 }
