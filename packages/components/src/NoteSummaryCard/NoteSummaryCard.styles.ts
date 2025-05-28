f1c29c93aa29cfc764b31e0b31061312202736cf
Danny Wieser <danny.wieser@gmail.com>
2025-05-28 07:00:54 -0600

    no thumbnails on dashboard

--- packages/components/src/NoteSummaryCard/NoteSummaryCard.styles.ts
+++ packages/components/src/NoteSummaryCard/NoteSummaryCard.styles.ts
@@ -3,26 +3,9 @@ import { createUseStyles } from 'react-jss'
 import { spacing } from '../theme/theme'
 import { MarkdownMemoryTheme } from '../theme/theme.types'
 
-export const imageHeight = 75
-
 const styles = createUseStyles<string, undefined, MarkdownMemoryTheme>(
   (theme) => {
-    const border = `2px solid ${theme.colors.slate[600]}`
     return {
-      image: {
-        border,
-        borderRadius: spacing(theme, 2),
-        height: `${imageHeight}px`,
-        overflow: 'hidden',
-      },
-      imageStack: {
-        borderTop: `1px solid ${theme.colors.slate[200]}`,
-        display: 'grid',
-        gap: spacing(theme, 2),
-        gridTemplateColumns: 'repeat( auto-fit, minmax(20px, 75px) )',
-        overflow: 'hidden',
-        paddingTop: spacing(theme, 2),
-      },
       list: {
         listStyleType: 'none',
         margin: 0,
--- packages/components/src/NoteSummaryCard/NoteSummaryCard.tsx
+++ packages/components/src/NoteSummaryCard/NoteSummaryCard.tsx
@@ -1,18 +1,11 @@
 import Card from '../Card/Card'
-import Image from '../Image/Image'
 import Text from '../Text/Text'
-import useStyles, { imageHeight } from './NoteSummaryCard.styles'
+import useStyles from './NoteSummaryCard.styles'
 import { NoteSummaryCardProps } from './NoteSummaryCard.types'
 
 export default function NoteSummaryCard(props: NoteSummaryCardProps) {
   const { cardName, href, icon, notes } = props
   const styles = useStyles()
-
-  const files = notes
-    ? notes.reduce<string[]>((acc, { filePaths = [] }) => {
-        return [...acc, ...filePaths]
-      }, [])
-    : []
 
   const ListNoteTitles = () =>
     notes && (
@@ -29,15 +22,6 @@ export default function NoteSummaryCard(props: NoteSummaryCardProps) {
     <Card href={href} icon={icon} title={cardName}>
       {notes && notes.length === 0 && <Text variant="em">No notes found</Text>}
       <ListNoteTitles />
-      {files && files.length ? (
-        <div className={styles.imageStack}>
-          {files.map((file) => (
-            <div className={styles.image}>
-              <Image h={imageHeight} href={file} op="resize" />
-            </div>
-          ))}
-        </div>
-      ) : null}
     </Card>
   )
 }
