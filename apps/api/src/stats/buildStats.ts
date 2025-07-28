b1e269f70707e0ee0e3004a2e71af169f60d5fa7
Danny Wieser <danny.wieser@gmail.com>
2025-07-28 08:18:05 -0600

    feat: entries created and modified per day

--- apps/api/src/stats/buildStats.ts
+++ apps/api/src/stats/buildStats.ts
@@ -1,15 +1,43 @@
 import { Stats } from '@markdown-memory/services'
+import { fmtDate } from '@markdown-memory/utilities'
 import { RedisClientType } from 'redis'
 
 import { getAllNotes } from '../notes/util'
+import { NoteResponse } from '../types'
+
+const createDateMap = (notes: NoteResponse[]) => {
+  const dateMap = {}
+  notes.forEach(({ created, modified }) => {
+    const createDate = fmtDate(new Date(created))
+    const modifiedDate = fmtDate(new Date(modified))
+
+    const findDate = (searchDate: string) =>
+      dateMap[searchDate] ?? { createdCount: 0, modifiedCount: 0 }
+
+    // bump count for created based on this notes create date
+    const createdEntry = findDate(createDate)
+    dateMap[createDate] = {
+      ...createdEntry,
+      createdCount: createdEntry.createdCount + 1,
+    }
+
+    // bump count for created based on this notes create date
+    const modifiedEntry = findDate(modifiedDate)
+    dateMap[modifiedDate] = {
+      ...modifiedEntry,
+      modifiedCount: modifiedEntry.modifiedCount + 1,
+    }
+  })
+
+  return dateMap
+}
 
 export default async function buildStats(
   redis: RedisClientType
 ): Promise<Stats> {
   const allNotes = await getAllNotes(redis)
-  const dateMap = new Map()
   return {
-    entriesByDay: dateMap,
+    entriesByDay: createDateMap(allNotes),
     totalEntries: allNotes.length,
     totalTags: 0,
   }
--- packages/services/src/stats/stats.types.ts
+++ packages/services/src/stats/stats.types.ts
@@ -1,5 +1,10 @@
+export interface DateCreatedAndModified {
+  createdCount: number
+  modifiedCount: number
+}
+
 export interface Stats {
-  entriesByDay: Map<string, number>
+  entriesByDay: { [key: string]: DateCreatedAndModified }
   totalEntries: number
   totalTags: number
 }
